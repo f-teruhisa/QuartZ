d631694df24c55c447b242ec3b3768c7f4215a9a
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-03-10 14:27:52 +0900

    add group_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'groups/create'
+  get 'groups/new'
+  get 'groups/show'
+  get 'membergroup_association/create'
+  get 'membergroup_association/new'
+  get 'membergroup_association/show'
   root to: 'index#show'
   devise_for :members
   resources :organizations do
