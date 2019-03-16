be7abcf41cc05f7bfdbde8d5500641a9d6b2b3c1
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-03-16 19:07:10 +0900

    determine organization view

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,26 +1,16 @@
 class OrganizationsController < ApplicationController
   before_action :set_organization, only: %i[show edit update destroy]
 
-  # GET /organizations
-  # GET /organizations.json
-  def index
-    @organizations = Organization.all
-  end
+  def index; end
 
-  # GET /organizations/1
-  # GET /organizations/1.json
   def show; end
 
-  # GET /organizations/new
   def new
     @organization = Organization.new
   end
 
-  # GET /organizations/1/edit
   def edit; end
 
-  # POST /organizations
-  # POST /organizations.json
   def create
     @organization = Organization.new(organization_params)
 
@@ -35,8 +25,6 @@ class OrganizationsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /organizations/1
-  # PATCH/PUT /organizations/1.json
   def update
     respond_to do |format|
       if @organization.update(organization_params)
@@ -49,8 +37,6 @@ class OrganizationsController < ApplicationController
     end
   end
 
-  # DELETE /organizations/1
-  # DELETE /organizations/1.json
   def destroy
     @organization.destroy
     respond_to do |format|
@@ -61,12 +47,10 @@ class OrganizationsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_organization
     @organization = Organization.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def organization_params
     params.require(:organization).permit(:name, :url, :image_url, :text)
   end
