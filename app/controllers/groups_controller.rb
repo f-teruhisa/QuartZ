1be398dbd6d31f6c0854f0c8acad1bb3b6ca5da2
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-03-31 16:24:28 +0900

    fix groups show view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,15 @@
 class GroupsController < ApplicationController
+  before_action :load_resouces, only: %i[show]
   def create; end
 
   def new; end
 
-  def show; end
+  def show;end
+
+  def load_resouces
+    @group = Group.find(params[:id])
+    @lunch = Lunch.find(params[:lunch_id])
+    @organization = Organization.find(@lunch.organization_id)
+    @members = Member.group_members(@group.id)
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -6,4 +6,9 @@ class Member < ApplicationRecord
   has_many :member_organization_associations, dependent: :destroy
   has_many :member_group_associations, dependent: :destroy
   has_many :organizations, through: :member_organization_associations
+
+  scope :group_members, -> group_id {
+    joins(:member_group_associations).where('member_group_associations.group_id = ?', group_id)
+  }
+
 end
