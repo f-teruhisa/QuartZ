f59a0ac7b74fa7e6145d8fce7eb08a317a19cdde
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-03-31 19:17:42 +0900

    add update group method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,10 +6,21 @@ class GroupsController < ApplicationController
 
   def show;end
 
+  def update
+    group = Group.find(params[:id])
+    group.update_attributes(group_params)
+    redirect_to action: :show
+  end
+
   def load_resouces
     @group = Group.find(params[:id])
     @lunch = Lunch.find(params[:lunch_id])
     @organization = Organization.find(@lunch.organization_id)
     @members = Member.group_members(@group.id)
+  end
+
+  private
+   def group_params
+      params.require(:group).permit(:comment, :url, :image_url)
   end
 end
