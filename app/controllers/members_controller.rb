f1b3aee475a978d648f4e0dce08af0888b3158b2
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-03-31 16:24:51 +0900

    add member show action controller view

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,3 @@
+class MembersController < ApplicationController
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   root to: 'index#show'
   devise_for :members
+  resources :members, :only => [:show] # ユーザーの詳細画面のためのrouting
   resources :member_organization_association
   resources :organizations do
     resources :lunch, only: %i[index create show] do
