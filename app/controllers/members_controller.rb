ccc199ecd111075b7754b202f669fce1f5fdfdc5
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-04-06 17:50:31 +0900

    create user edit & update

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -6,7 +6,11 @@ class MembersController < ApplicationController
     redirect_to :action => "show" unless same_member?
   end
 
-  def update; end
+  def update
+    member = Member.find(params[:id])
+    member.update(update_params)
+    redirect_to :action => "show"
+  end
 
   private
 
@@ -19,5 +23,9 @@ class MembersController < ApplicationController
     @member = Member.find(params[:id])
     # current_memberをViewで複数回使用する際に、DBを複数スキャンすることを防ぐ
     current_member.nil? ? @current_member = User.find_by(id: session[:user_id]) : @current_member
+  end
+
+  def update_params
+    params.require(:member).permit(:name, :email, :text, :image_url)
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,7 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  mount_uploader :image_url, ImageUploader
   has_many :member_organization_associations, dependent: :destroy
   has_many :member_group_associations, dependent: :destroy
   has_many :organizations, through: :member_organization_associations
