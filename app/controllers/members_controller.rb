27f3703bf4d67d015e24fe44d5d803b553d71796
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-04-06 17:08:39 +0900

    refactor current_member scan

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,13 +1,13 @@
 class MembersController < ApplicationController
   before_action :load_resouces, only: [:show]
   def show
-    # current_memberをViewで複数回使用する際に、DBを複数スキャンすることを防ぐ
-    current_member.nil? ? @current_member = User.find_by(id: session[:user_id]) : @current_member
   end
 
   def edit; end
 
   def load_resouces
     @member = Member.find(params[:id])
+    # current_memberをViewで複数回使用する際に、DBを複数スキャンすることを防ぐ
+    current_member.nil? ? @current_member = User.find_by(id: session[:user_id]) : @current_member
   end
 end
