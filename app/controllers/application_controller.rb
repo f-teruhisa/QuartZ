5750fd7c2f1fec5a8c09925530b43f3e71911890
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-04-07 13:52:27 +0900

    fix devise routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # deviseでのログイン後の遷移先を指定
+  def after_sign_in_path_for(resource)
+      organizations_path
+  end
 end
