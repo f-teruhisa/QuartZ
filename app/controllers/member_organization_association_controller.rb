dc344bbb389b5969ab9e19bac6cc68e148456161
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-04-07 23:26:14 +0900

    add invite view

--- app/controllers/member_organization_association_controller.rb
+++ app/controllers/member_organization_association_controller.rb
@@ -1,13 +1,22 @@
 class MemberOrganizationAssociationController < ApplicationController
-  before_action :load_resources, only: [:index]
+  before_action :load_resources, only: [:index, :new]
   def index;end
 
-  def create;end
+  def create
+    @association = MemberOrganizationAssociation.create(create_association_params)
+    redirect_to action: :new, flash: { success: 'Success!!' }
+  end
 
-  def new;end
+  def new; end
+
+  private
 
   def load_resources
     @organization = Organization.find(params[:organization_id])
     @members = Member.organization_members(@organization.id)
+  end
+
+  def create_association_params
+    params.require(:member_organization_association).permit(:member_id, :group_id)
   end
 end
