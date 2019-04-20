34663df04d76e2be6296b178b34a2d7828e07f58
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-04-20 14:50:36 +0900

    fix member logout loginc

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   root to: 'index#show'
   devise_for :members, controllers: { omniauth_callbacks: 'members/omniauth_callbacks' }
+  devise_scope :member do
+    get '/members/sign_out' => 'devise/sessions#destroy'
+  end
   resources :members, :only => [:show, :edit, :update] # ユーザーの詳細画面のためのrouting
   resources :organizations, :only => [:create, :new]
   resources :organizations, :only => [:index, :show, :edit, :update] do
