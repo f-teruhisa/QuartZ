46aea97345664ba88a15d0246ca7a65bc005c13b
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-11 19:19:58 +0900

    do with rubocop

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 describe OrganizationsController, type: :controller do
   describe '#create' do
-    let(:params) do {
-      organization: {
-        name: 'name',
-        url: 'https://example.com',
-        image_url: 'example.png',
-        text: 'text'
-      }
-    }
-    end
-
     context 'normal' do
+      let(:params) do
+        {
+          organization: {
+            name: 'name',
+            url: 'https://example.com',
+            image_url: 'example.png',
+            text: 'text'
+          }
+        }
+      end
+
       it 'Organizations are created' do
         expect { post :create, params: params }.to change { Organization.count }.by(1)
       end
