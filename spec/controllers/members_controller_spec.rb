e47d8b4f6cc0c705160468419311d6300460854c
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-24 18:23:16 +0900

    add member controller spec show normal context spec test

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -3,11 +3,10 @@ require 'login_helper'
 
 describe MembersController, type: :controller do
   describe '#show' do
-    before do
-      @member = create(:member)
-    end
+    let(:member) { create(:member) }
+
     context 'normal' do
-      let(:params) { {id: @member.id} }
+      let(:params) { {id: member.id} }
 
       it 'returns status 200' do
         get :show, params: params
@@ -16,7 +15,7 @@ describe MembersController, type: :controller do
 
       it 'request member was assigned to instance @member' do
         get :show, params: params
-        expect(assigns(:member)).to eq @member
+        expect(assigns(:member)).to eq member
       end
 
       it 'returns show template' do
