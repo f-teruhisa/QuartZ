0b4930b680f840b83e779f8d031190570c42113e
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-24 23:30:33 +0900

    add index spec test into member_organizations_controller

--- spec/controllers/member_organization_associations_controller_spec.rb
+++ spec/controllers/member_organization_associations_controller_spec.rb
@@ -3,6 +3,33 @@ require 'rails_helper'
 describe MemberOrganizationAssociationController, type: :controller do
   let(:member) { create(:member) }
   let(:organization) { create(:organization) }
+
+  describe '#index' do
+    context 'normal' do
+      let(:params) { { organization_id: organization.id } }
+      let(:member_organization_association) do
+        {
+          member_id: member.id,
+          organization_id: organization.id
+        }
+      end
+
+      it 'return 200 status response' do
+        get :index, params: params
+        expect(response.status).to eq(200)
+      end
+
+      it 'assign resources' do
+        get :index, params: params
+        expect(assigns(:organization)).to eq organization
+      end
+
+      it 'return index template' do
+        get :index, params: params
+        expect(response).to render_template :index
+      end
+    end
+  end
   describe '#create' do
     context 'normal' do
       let(:params) { { member_id: member.id, organization_id: organization.id } }
