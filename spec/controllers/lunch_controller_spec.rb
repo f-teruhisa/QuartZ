017381be648de92c6bbf642f089f55f8587597b8
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-24 23:14:35 +0900

    add spec test abnormal index test into lunch controller spec

--- spec/controllers/lunch_controller_spec.rb
+++ spec/controllers/lunch_controller_spec.rb
@@ -8,6 +8,8 @@ describe LunchController, type: :controller do
   describe '#index' do
     context 'normal' do
       let(:params) { {organization_id: organization.id} }
+      let(:lunch) { create(:lunch, organization_id: organization.id ) }
+
       it 'return status 200' do
         get :index, params: params
         expect(response.status).to eq(200)
@@ -16,11 +18,22 @@ describe LunchController, type: :controller do
       it 'assigns resouces' do
         get :index, params: params
         expect(assigns(:organization)).to eq organization
+        expect(assigns(:lunches)).to contain_exactly lunch
       end
 
       it 'return index template' do
         get :index, params: params
         expect(response).to render_template :index
+      end
+    end
+
+    context 'normal' do
+      let(:params) { {organization_id: organization.id.to_i + 1} }
+
+      it 'return status 200' do
+        expect {
+          get :index, params: params
+        }.to raise_exception(ActiveRecord::RecordNotFound)
       end
     end
   end
