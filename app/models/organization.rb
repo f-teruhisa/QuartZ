641e3cb79b62a780823564551e71374820f0d755
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-25 23:07:43 +0900

    add spec test into organization model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,8 @@ class Organization < ApplicationRecord
   mount_uploader :image_url, ImageUploader
   has_many :members, through: :member_organization_associations
   has_many :member_organization_associations, dependent: :destroy
+
+  validates :name, presence: true
+  validates :url, presence: true
+  validates :numbers_of_groups, presence: true
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
+  describe 'organization validates' do
+  end
 end
