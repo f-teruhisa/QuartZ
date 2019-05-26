2ec65af0562711aded6a0f62423e6610b8798b81
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-26 19:05:23 +0900

    add validates into lunch model

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -2,6 +2,8 @@ class Lunch < ApplicationRecord
   belongs_to :organization
   has_many :groups, dependent: :destroy
 
+  validates :organization_id, presence: true, numericality: true
+
   def create_group_with_rank!(rank)
     groups.create!(rank: rank)
   end
