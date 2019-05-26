c548e302017933c6862b078167366d0740022754
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-26 19:12:44 +0900

    add spec test and validates into member_organization_assoiciations

--- app/models/member_organization_association.rb
+++ app/models/member_organization_association.rb
@@ -1,4 +1,9 @@
 class MemberOrganizationAssociation < ApplicationRecord
   belongs_to :member
   belongs_to :organization
+
+  validates :member_id, presence: true, numericality: true
+  validates :organization_id, presence: true, numericality: true
+  validates :organizer, inclusion: { in: [true, false] }
+
 end
--- spec/models/member_organization_association_spec.rb
+++ spec/models/member_organization_association_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe MemberOrganizationAssociation, type: :model do
+  describe 'member validates' do
+    it 'is invalid without organization_id' do
+      @member_organization_association = MemberOrganizationAssociation.new(organization_id: nil)
+      expect(@member_organization_association.valid?).to eq(false)
+    end
+
+    it 'is invalid without member_id' do
+      @member_organization_association = MemberOrganizationAssociation.new(member_id: nil)
+      expect(@member_organization_association.valid?).to eq(false)
+    end
+
+    it 'is invalid without setting organizer' do
+      @member_organization_association = MemberOrganizationAssociation.new(organizer: nil)
+      expect(@member_organization_association.valid?).to eq(false)
+    end
+  end
 end
