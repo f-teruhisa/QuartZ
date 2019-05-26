9f7e4ed166d643acab90d76b959191a15108a34d
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-26 17:16:55 +0900

    add organization model spec validations test

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,5 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
   describe 'organization validates' do
+    it 'is invalid without a name' do
+      @organization = Organization.new(name: nil)
+      expect(@organization.valid?).to eq(false)
+    end
+
+    it 'is invalid without url' do
+      @organization = Organization.new(url: nil)
+      expect(@organization.valid?).to eq(false)
+    end
+
+    it 'is invalid without settings numbers_of_groups' do
+      @organization = Organization.new(numbers_of_groups: nil)
+      expect(@organization.valid?).to eq(false)
+    end
   end
 end
