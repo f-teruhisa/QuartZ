4ae5c9a2c8b5fc9fc9313fb4640bc17d2fb4efdf
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-26 18:42:01 +0900

    add lunch model validation spec

--- spec/models/lunch_spec.rb
+++ spec/models/lunch_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Lunch, type: :model do
+  describe 'lunch validates' do
+    context 'normal' do
+      it 'is invalid without organization_id' do
+        @lunch = Lunch.new(organization_id: nil)
+        expect(@lunch.valid?).to eq(false)
+      end
+    end
+  end
 end
