96c96009cfaba174921a3ae80253a103e4f704a2
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-06-01 22:30:42 +0900

    add rambulance error files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # deviseでのログイン後の遷移先を指定
+  # Redirect setting for devise after sign in
   def after_sign_in_path_for(_resource)
     '/organizations'
   end
