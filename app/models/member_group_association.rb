bcdc263d3675cd63f1f9a23ccf2a313c04f90efa
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-06-08 16:34:20 +0900

    add model spec into member group asssociations

--- app/models/member_group_association.rb
+++ app/models/member_group_association.rb
@@ -1,4 +1,7 @@
 class MemberGroupAssociation < ApplicationRecord
   belongs_to :member
   belongs_to :group
+
+  validates :member_id, presence: true, numericality: true
+  validates :group_id, presence: true, numericality: true
 end
--- spec/models/member_group_association_spec.rb
+++ spec/models/member_group_association_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe MemberGroupAssociation, type: :model do
+  describe 'member group validates' do
+    it 'is invalid without member_id' do
+      @member_group_association = MemberGroupAssociation.new(member_id: nil)
+      expect(@member_group_association.valid?).to eq(false)
+    end
+
+    it 'is invalid without group_id' do
+      @member_group_association = MemberGroupAssociation.new(group_id: nil)
+      expect(@member_group_association.valid?).to eq(false)
+    end
+  end
 end
