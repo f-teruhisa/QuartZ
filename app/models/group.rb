e8875094c127ab35758f8f137b8246f722af87a4
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-06-09 17:28:14 +0900

    fix model validate NOT unique rank

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,7 +6,7 @@ class Group < ApplicationRecord
   has_many :member_group_associations, dependent: :destroy
 
   validates :lunch_id, presence: true, numericality: true
-  validates :rank, presence: true, uniqueness: true, numericality: true
+  validates :rank, presence: true, numericality: true
 
   def create_member_group_association!(member)
     member_group_associations.create!(member_id: member.id)
