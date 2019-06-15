754f0dcab1a2213ce14bb8222d0690845fb35fca
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-06-15 19:15:08 +0900

    add normal spec test into lunch_spec

--- spec/models/lunch_spec.rb
+++ spec/models/lunch_spec.rb
@@ -3,6 +3,16 @@ require 'rails_helper'
 RSpec.describe Lunch, type: :model do
   describe 'lunch validates' do
     context 'normal' do
+      let(:organization) { create(:organization) }
+      it 'is valid lunch' do
+        lunch = Lunch.new(
+          organization_id: organization.id
+        )
+        expect(lunch.valid?).to eq(true)
+      end
+    end
+
+    context 'abnormal' do
       it 'is invalid without organization_id' do
         lunch = Lunch.new(organization_id: nil)
         expect(lunch.valid?).to eq(false)
