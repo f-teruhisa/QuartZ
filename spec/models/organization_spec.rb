210f3cd58bf8626eb3f890ac903dbb5eadc626ec
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-06-15 18:43:33 +0900

    add normal valid spec test in organization model

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -4,30 +4,30 @@ RSpec.describe Organization, type: :model do
   describe 'organization validates' do
     context 'normal' do
       it 'is valid organization' do
-        @organization = Organization.new(
+        organization = Organization.new(
           name: Faker::Name.name,
           url: Faker::Internet.url,
           image_url: Faker::Internet.url + '.png',
           text: Faker::Lorem.sentence,
           numbers_of_groups: Faker::Number.number(1)
         )
-        expect(@organization.valid?).to eq(true)
+        expect(organization.valid?).to eq(true)
       end
     end
     context 'abnormal' do
       it 'is invalid without a name' do
-        @organization = Organization.new(name: nil)
-        expect(@organization.valid?).to eq(false)
+        organization = Organization.new(name: nil)
+        expect(organization.valid?).to eq(false)
       end
 
       it 'is invalid without url' do
-        @organization = Organization.new(url: nil)
-        expect(@organization.valid?).to eq(false)
+        organization = Organization.new(url: nil)
+        expect(organization.valid?).to eq(false)
       end
 
       it 'is invalid without settings numbers_of_groups' do
-        @organization = Organization.new(numbers_of_groups: nil)
-        expect(@organization.valid?).to eq(false)
+        organization = Organization.new(numbers_of_groups: nil)
+        expect(organization.valid?).to eq(false)
       end
     end
   end
