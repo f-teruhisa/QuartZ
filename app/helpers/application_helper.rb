52e942373ff10d01493134d24b7c3eeab0e77ee3
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-06-30 17:34:53 +0900

    fix rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,35 +1,37 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # rubocop:disable Metrics/MethodLength
   def default_meta_tags
     {
-      title:       "QuartZ",
-      description: "QuartZ solves communication challenges in your organization with random lunch.",
-      keywords:    "random,lunch,communication,team,organization",
+      title:       'QuartZ',
+      description: 'QuartZ solves communication challenges in your organization with random lunch.',
+      keywords:    'random,lunch,communication,team,organization',
       icon: [
         { href: image_url('favicon.ico') },
-        { href: image_url('top_image.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+        { href: image_url('top_image.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' }
       ],
-      noindex: ! Rails.env.production?,
-	  canonical: request.original_url,
-      charset: "UTF-8",
+      noindex: !Rails.env.production?,
+      canonical: request.original_url,
+      charset: 'UTF-8',
       og: {
         title: :title,
         description: :description,
-        type: "website",
+        type: 'website',
         url: request.original_url,
-        image: image_url("top_image.png"),
-        site_name: "site name",
-        locale: "ja_JP"
+        image: image_url('top_image.png'),
+        site_name: 'site name',
+        locale: 'ja_JP'
       },
       twitter: {
         site: '@terry_i_',
         card: 'summary',
-        image: image_url("top_image.png")
-	  },
+        image: image_url('top_image.png')
+      },
       fb: {
         app_id: '2334858459911814'
       }
     }
   end
+  # rubocop:enable Metrics/MethodLength
 end
